f966c937d9d69f38d953e046732b770e1a80abcb
Goodness Duru <goodyduru@gmail.com>
2023-09-21 17:33:01 +0100

    Add unidirectional Python implementations

--- python/named-pipes/client.py
+++ python/named-pipes/client.py
@@ -1,22 +1,20 @@
 import os
-import time
+
 
 ROUNDS = 100
 
 
 def run():
     pipe_path = '/tmp/ping'
-    fd = os.open(pipe_path, os.O_RDWR)
-    data = os.read(fd, 4).decode()
-    while data != 'end':
-        print(f"Client: Received {data}")
-        os.write(fd, b"pong")
-        print(f"Client: Sent pong")
-        time.sleep(0.1)
-        data = os.read(fd, 4).decode()
+    fd = os.open(pipe_path, os.O_WRONLY)
+    i = 0
+    while i != ROUNDS:
+        os.write(fd, b'ping')
+        print("Client: Sent ping")
+        i += 1
+    os.write(fd, b'end')
     os.close(fd)
 
 
 if __name__ == "__main__":
-    time.sleep(2)
     run()
--- python/named-pipes/server.py
+++ python/named-pipes/server.py
@@ -1,27 +1,17 @@
 import os
-import time
-
-ROUNDS = 100
 
 
 def run():
     pipe_path = '/tmp/ping'
-    os.mkfifo(pipe_path, 416)
-    fd = os.open(pipe_path, os.O_RDWR)
-    i = 0
-    while i != ROUNDS:
-        print("Server: Sent ping")
-        os.write(fd, b'ping')
-        time.sleep(0.1)
-        data = os.read(fd, 4).decode()
+    os.mkfifo(pipe_path)
+    fd = os.open(pipe_path, os.O_RDONLY)
+    data = os.read(fd, 4).decode()
+    while data != 'end':
         print(f"Server: Received {data}")
-        i += 1
-    os.write(fd, b'end')
-        
+        data = os.read(fd, 4).decode()
     os.close(fd)
     os.unlink(pipe_path)
 
 
 if __name__ == "__main__":
-    time.sleep(1)
     run()
